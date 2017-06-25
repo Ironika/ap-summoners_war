0492a5ebf5e01e08a8c852ea579d16954cbd5da5
I051486 <antoine.maillard@sap.com>
2017-06-25 11:12:27 +0200

    diff from generator

---
+++ front/src/components/Home.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import HomeData from 'components/HomeData'
+import Login from 'components/lib/login/Login'
+import Register from 'components/lib/register/Register'
+import './Home.scss'
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    componentWillMount() {
+        HomeData.register(this)
+    }
+
+    componentWillUnmount() {
+        HomeData.unregister()
+    }
+
+    buildData() {
+        switch (this.state.status) {
+            case HomeData.STATUS.HOME_LOGIN: 
+                return (<Login onClick={this.onSwitchMode.bind(this)}/>)
+            case HomeData.STATUS.HOME_REGISTER: 
+                return (<Register onClick={this.onSwitchMode.bind(this)}/>)
+        }
+    }
+
+    render() {
+        return (
+            <div className='ap-home'>
+                <div className="row">
+                    <div className="col-xs-12 col-md-6">
+                        <div className="sm-sheet">
+                            <div className="row">
+                                {this.buildData()}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xs-12 col-md-6">
+                        <div className="sm-sheet">
+                            <p className="sm-home-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                        </div>
+                        <img className="sm-home-img" src={"assets/images/summonerswar.png"} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Home
--- front/src/components/Routes.js
+++ front/src/components/Routes.js
@@ -1,5 +1,5 @@
 import App from 'components/App'
-import Home from 'components/home/Home'
+import Home from 'components/Home'
 import Profile from 'components/profile/Profile'
 import Monsters from 'components/monsters/Monsters'
 import Runes from 'components/runes/Runes'
@@ -7,12 +7,12 @@ import Import from 'components/import/Import'
 import Builds from 'components/builds/Builds'
 
 let appRoutes = [
-	{  component: Home },
-	{  path: '/profile', component: Profile },
-	{  path: '/monsters', component: Monsters },
-	{  path: '/runes', component: Runes },
-	{  path: '/import', component: Import },
-	{  path: '/builds', component: Builds },
+	{ component: Home },
+	{ path: '/profile', component: Profile },
+	{ path: '/monsters', component: Monsters },
+	{ path: '/runes', component: Runes },
+	{ path: '/import', component: Import },
+	{ path: '/builds', component: Builds },
 ]
 let routes = [
 	{ path: '/', component: App, routes: appRoutes },
