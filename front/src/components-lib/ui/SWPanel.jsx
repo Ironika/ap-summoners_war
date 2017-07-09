b197f78283cb4176bf448cd20f8d1d3c07221d6e
I051486 <antoine.maillard@sap.com>
2017-07-09 13:47:04 +0200

    added sw panel

---
+++ front/src/components-lib/ui/SWPanel.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './SWPanel.scss'
+
+class SWPanel extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() { return (
+        <div className={'sm-panel ' + (this.props.size ? ' sm-panel-' + this.props.size : 'sm-panel-sm') +  (this.props.className ? ' ' + this.props.className : '')}>
+            <div className='sm-panel-border sm-panel-border-top' />
+            <div className='sm-panel-border sm-panel-border-right' />
+            <div className='sm-panel-border sm-panel-border-bottom' />
+            <div className='sm-panel-border sm-panel-border-left' />
+            <div className='sm-panel-corner sm-panel-corner-tr' />
+            <div className='sm-panel-corner sm-panel-corner-br' />
+            <div className='sm-panel-corner sm-panel-corner-bl' />
+            <div className='sm-panel-corner sm-panel-corner-tl' />
+            {this.props.children}
+        </div>
+    )}
+}
+export default SWPanel
