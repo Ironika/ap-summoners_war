927be3cf60cf54a5d9292cf54e9525435152a60d
matthieu <matthieu@mail.com>
2017-07-23 19:39:25 +0200

    build queue

--- server/src/main/java/org/ap/summonerwar/helpers/DoBuildHelper.java
+++ server/src/main/java/org/ap/summonerwar/helpers/DoBuildHelper.java
@@ -1,6 +1,10 @@
 package org.ap.summonerwar.helpers;
 
 import javax.ws.rs.core.SecurityContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.core.Response.Status;
 
 
@@ -16,21 +20,46 @@ import org.ap.web.internal.APWebException;
 
 public class DoBuildHelper {
 
+	public static Thread thread = null;
+	public static List<DoBuildBean> buildQueue = new ArrayList<DoBuildBean>();
+	public static final Object lock = new Object();
+	
 	public static Object postDoBuild(SecurityContext sc, DoBuildBean doBuildBean) throws APWebException {
 		
-		try {
-			BuildData buildData = BuildCollection.getById(doBuildBean.buildId);
-			buildData.state = "InBuilding";
-			BuildCollection.update(buildData);
-			ApauthData dataAuth = ApauthCollection.getByUsername(sc.getUserPrincipal().getName());
-			UserData userData = UserCollection.getById(dataAuth.getEntityId());
-			Optimizer.build(userData.getId(), doBuildBean);
-			buildData.state = "Build";
-			BuildCollection.update(buildData);
-		} catch (Exception e) {
-			throw new APWebException(e.getMessage(), "500", Status.INTERNAL_SERVER_ERROR);
+		synchronized (DoBuildHelper.lock) {
+			DoBuildHelper.buildQueue.add(doBuildBean);
 		}
+		System.out.println("Add Build: " + doBuildBean.buildId);
 		
+		if (DoBuildHelper.thread == null) {
+			DoBuildHelper.thread = new Thread() {
+				public void run() {
+					try {
+						while(true) {
+							DoBuildBean currentDoBuildBean = null;
+							synchronized (DoBuildHelper.lock) {
+								if (DoBuildHelper.buildQueue.size() == 0) {
+									DoBuildHelper.thread = null;
+									break;
+								}
+								currentDoBuildBean = DoBuildHelper.buildQueue.remove(0);
+							}
+							BuildData buildData = BuildCollection.getById(currentDoBuildBean.buildId);
+							buildData.state = "InBuilding";
+							BuildCollection.update(buildData);
+							ApauthData dataAuth = ApauthCollection.getByUsername(sc.getUserPrincipal().getName());
+							UserData userData = UserCollection.getById(dataAuth.getEntityId());
+							Optimizer.build(userData.getId(), currentDoBuildBean);
+							buildData.state = "Build";
+							BuildCollection.update(buildData);
+						}
+					} catch (Exception e) {
+						System.err.println(e.getMessage());
+					}
+				}
+			};
+			DoBuildHelper.thread.start();
+		}
 		return null;
 	}
 
